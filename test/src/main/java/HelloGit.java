639546a7d450d7d4dae4bf6eb5634f8e64959fe5
tom_glb <goodMorning_glb@test.com>
2021-03-18 15:51:15 +0800

    克隆人员，协同开发添加

--- test/src/main/java/HelloGit.java
+++ test/src/main/java/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("Hello 主干添加，测试冲突");
         System.out.println("Hello dev添加，测试冲突");
         System.out.println("Hello 在线添加");
+        System.out.println("Hello 克隆人员添加");
     }
 }
